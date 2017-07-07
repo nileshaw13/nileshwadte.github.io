83e3101e3a5698bb528123633287fb8f49c794f5
nileshaw13 <nileshaw13@gmail.com>
2017-07-07 19:11:40 +0530

    07-07-17
    Without_Animation

---
+++ js/index_2.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('#myTab a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});
+
+$('#myTab a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
